54799f4aa5f02a4257a44fc0bd13fa62a930b46f
Baymaxff <143253884+Baymaxff@users.noreply.github.com>
2025-01-01 12:32:27 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,16 +13,16 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `╭━━〔 *KHAN-AI* 〕━━┈⊷
+        const status = `╭━━〔 *𝙆𝘼𝙑𝙄-𝙀𝙓𝙀-𝙑1* 〕━━┈⊷
 ┃◈╭─────────────·๏
 ┃◈┃• *⏳Uptime*:  ${runtime(process.uptime())} 
 ┃◈┃• *📟 Ram usage*: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${(os.totalmem() / 1024 / 1024).toFixed(2)}MB
 ┃◈┃• *⚙️ HostName*: ${os.hostname()}
-┃◈┃• *👨‍💻 Owner*: Jawad TechX
+┃◈┃• *👨‍💻 Owner*: 𝙆𝘼𝙑𝙄-𝙀𝙓𝙀-𝙑1
 ┃◈┃• *🧬 Version*: 2.0.0
 ┃◈└───────────┈⊷
 ╰──────────────┈⊷
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX`;
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙆𝘼𝙑𝙄-𝙀𝙓𝙀-𝙑1 `;
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
@@ -34,7 +34,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363354023106228@newsletter',
-                    newsletterName: 'JawadTechX',
+                    newsletterName: '𝙆𝘼𝙑𝙄-𝙀𝙓𝙀-𝙑1',
                     serverMessageId: 143
                 }
             }
