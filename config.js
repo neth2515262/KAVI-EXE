661f8c9e73619e416d6b02a4e759b736ef57ff15
Baymaxff <143253884+Baymaxff@users.noreply.github.com>
2025-01-13 00:54:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "gVhHjQ5D#jB4uA49GVTTX1UIR05nUvklSvetgK1HK4hb7saug2YA",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=Fwh03CqB#JXWcg4050yU8forLX0PYATEfgFG-lQnVx26FAux-PeM",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/5vp4rr.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "KAVI EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 KAVI_EXE📘☀",
